650961e747310506d838eab5ddb0a15e11d4aa39
Benjamin Bonvalet <benjamin591997@hotmail.fr>
2019-02-13 10:38:16 +0100

    fin gallerie

--- src/components/PhotosPrestations.vue
+++ src/components/PhotosPrestations.vue
@@ -94,7 +94,7 @@ export default {
         {title: '1', url: '../../static/img/Chez Caroline et Marc Deledalle/1.jpg'},
         {title: '2', url: '../../static/img/Chez Caroline et Marc Deledalle/2.jpg'},
         {title: '3', url: '../../static/img/Chez Caroline et Marc Deledalle/3.jpg'},
-        {title: '4', url: '../../static/img/Chez Caroline et Marc Deledalle/4.JPG'},
+        {title: '4', url: '../../static/img/Chez Caroline et Marc Deledalle/4.jpg'},
         {title: '5', url: '../../static/img/Chez Caroline et Marc Deledalle/5.jpg'},
         {title: '6', url: '../../static/img/Chez Caroline et Marc Deledalle/6.jpg'}
       ],
@@ -105,32 +105,32 @@ export default {
         {title: 'Tartelette au chocolat, nougatine aux pistaches, crème anglaise', url: '../../static/img/Cours de cuisine à domicile chez Mlle Mourait/4.jpg'}
       ],
       [
-        {title: '1', url: '../../static/img/Cours de cuisine chez Mme Sobry/1.jpg'},
-        {title: '2', url: '../../static/img/Cours de cuisine chez Mme Sobry/2.jpg'},
-        {title: '3', url: '../../static/img/Cours de cuisine chez Mme Sobry/3.jpg'},
-        {title: '4', url: '../../static/img/Cours de cuisine chez Mme Sobry/4.jpg'},
-        {title: '5', url: '../../static/img/Cours de cuisine chez Mme Sobry/5.jpg'},
-        {title: '6', url: '../../static/img/Cours de cuisine chez Mme Sobry/6.jpg'},
-        {title: '7', url: '../../static/img/Cours de cuisine chez Mme Sobry/7.jpg'}
+        {title: '1', url: '../../static/img/Cours de cuisine chez Mme Sobry/1.JPG'},
+        {title: '2', url: '../../static/img/Cours de cuisine chez Mme Sobry/2.JPG'},
+        {title: '3', url: '../../static/img/Cours de cuisine chez Mme Sobry/3.JPG'},
+        {title: '4', url: '../../static/img/Cours de cuisine chez Mme Sobry/4.JPG'},
+        {title: '5', url: '../../static/img/Cours de cuisine chez Mme Sobry/5.JPG'},
+        {title: '6', url: '../../static/img/Cours de cuisine chez Mme Sobry/6.JPG'},
+        {title: '7', url: '../../static/img/Cours de cuisine chez Mme Sobry/7.JPG'}
       ],
       [
-        {title: '1', url: '../../static/img/cours de cuisine chez Mr et Mme Varrasse-Vannesche/1.jpg'},
-        {title: '2', url: '../../static/img/cours de cuisine chez Mr et Mme Varrasse-Vannesche/2.jpg'},
-        {title: '3', url: '../../static/img/cours de cuisine chez Mr et Mme Varrasse-Vannesche/3.jpg'}
+        {title: '1', url: '../../static/img/cours de cuisine chez Mr et Mme Varrasse-Vannesche/1.JPG'},
+        {title: '2', url: '../../static/img/cours de cuisine chez Mr et Mme Varrasse-Vannesche/2.JPG'},
+        {title: '3', url: '../../static/img/cours de cuisine chez Mr et Mme Varrasse-Vannesche/3.JPG'}
       ],
       [
-        {title: 'Cours de cuisine chez Vanessa et Aurélien.', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/1.jpg'},
-        {title: "Flan d'épinards", url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/2.jpg'},
-        {title: "Mitonnée de veau aux écrevisses, flan d'épinards", url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/3.jpg'},
-        {title: 'Mousse de banane sur biscuit moelleux au chocolat, crème anglaise', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/4.jpg'},
-        {title: 'Préparation de la mitonnée de veau aux écrevisses', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/5.jpg'},
-        {title: 'Préparation de la mitonnée de veau aux écrevisses', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/6.jpg'},
-        {title: 'Préparation de la mitonnée de veau aux écrevisses', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/7.jpg'},
-        {title: 'Préparation de la mitonnée de veau aux écrevisses', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/8.jpg'},
-        {title: 'Préparation de la mousse de banane', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/9.jpg'},
-        {title: 'Préparation des ravioles', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/10.jpg'},
-        {title: 'Préparation du biscuit moelleux au chocolat', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/11.jpg'},
-        {title: 'Ravioles de Saint-Jacques aux poireaux, sauce safranée', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/12.jpg'}
+        {title: 'Cours de cuisine chez Vanessa et Aurélien.', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/1.JPG'},
+        {title: "Flan d'épinards", url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/2.JPG'},
+        {title: "Mitonnée de veau aux écrevisses, flan d'épinards", url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/3.JPG'},
+        {title: 'Mousse de banane sur biscuit moelleux au chocolat, crème anglaise', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/4.JPG'},
+        {title: 'Préparation de la mitonnée de veau aux écrevisses', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/5.JPG'},
+        {title: 'Préparation de la mitonnée de veau aux écrevisses', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/6.JPG'},
+        {title: 'Préparation de la mitonnée de veau aux écrevisses', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/7.JPG'},
+        {title: 'Préparation de la mitonnée de veau aux écrevisses', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/8.JPG'},
+        {title: 'Préparation de la mousse de banane', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/9.JPG'},
+        {title: 'Préparation des ravioles', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/10.JPG'},
+        {title: 'Préparation du biscuit moelleux au chocolat', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/11.JPG'},
+        {title: 'Ravioles de Saint-Jacques aux poireaux, sauce safranée', url: '../../static/img/cours de cuisine chez Vanessa et Aurélien/12.JPG'}
       ],
       [
         {title: '1', url: '../../static/img/Cours de cuisine entre amis à domicile/1.jpg'},
@@ -138,28 +138,28 @@ export default {
         {title: '3', url: '../../static/img/Cours de cuisine entre amis à domicile/3.jpg'}
       ],
       [
-        {title: '1', url: '../../static/img/Cours de cuisine pour enfants/1.jpg'},
-        {title: '2', url: '../../static/img/Cours de cuisine pour enfants/2.jpg'},
-        {title: '3', url: '../../static/img/Cours de cuisine pour enfants/3.jpg'},
-        {title: '4', url: '../../static/img/Cours de cuisine pour enfants/4.jpg'},
-        {title: '5', url: '../../static/img/Cours de cuisine pour enfants/5.jpg'},
-        {title: '6', url: '../../static/img/Cours de cuisine pour enfants/6.jpg'},
-        {title: '7', url: '../../static/img/Cours de cuisine pour enfants/7.jpg'},
-        {title: '8', url: '../../static/img/Cours de cuisine pour enfants/8.jpg'},
-        {title: '9', url: '../../static/img/Cours de cuisine pour enfants/9.jpg'},
-        {title: '10', url: '../../static/img/Cours de cuisine pour enfants/10.jpg'},
-        {title: '11', url: '../../static/img/Cours de cuisine pour enfants/11.jpg'},
-        {title: '12', url: '../../static/img/Cours de cuisine pour enfants/12.jpg'}
+        {title: '1', url: '../../static/img/Cours de cuisine pour enfants/1.JPG'},
+        {title: '2', url: '../../static/img/Cours de cuisine pour enfants/2.JPG'},
+        {title: '3', url: '../../static/img/Cours de cuisine pour enfants/3.JPG'},
+        {title: '4', url: '../../static/img/Cours de cuisine pour enfants/4.JPG'},
+        {title: '5', url: '../../static/img/Cours de cuisine pour enfants/5.JPG'},
+        {title: '6', url: '../../static/img/Cours de cuisine pour enfants/6.JPG'},
+        {title: '7', url: '../../static/img/Cours de cuisine pour enfants/7.JPG'},
+        {title: '8', url: '../../static/img/Cours de cuisine pour enfants/8.JPG'},
+        {title: '9', url: '../../static/img/Cours de cuisine pour enfants/9.JPG'},
+        {title: '10', url: '../../static/img/Cours de cuisine pour enfants/10.JPG'},
+        {title: '11', url: '../../static/img/Cours de cuisine pour enfants/11.JPG'},
+        {title: '12', url: '../../static/img/Cours de cuisine pour enfants/12.JPG'}
       ],
       [
-        {title: '1', url: '../../static/img/mariage de laurence et Eric 2013/1.jpg'},
-        {title: '2', url: '../../static/img/mariage de laurence et Eric 2013/2.jpg'},
-        {title: '3', url: '../../static/img/mariage de laurence et Eric 2013/3.jpg'}
+        {title: '1', url: '../../static/img/mariage de laurence et Eric 2013/1.JPG'},
+        {title: '2', url: '../../static/img/mariage de laurence et Eric 2013/2.JPG'},
+        {title: '3', url: '../../static/img/mariage de laurence et Eric 2013/3.JPG'}
       ],
       [
-        {title: '1', url: '../../static/img/profession de foi de Benjamin/1.jpg'},
-        {title: '2', url: '../../static/img/profession de foi de Benjamin/2.jpg'},
-        {title: '3', url: '../../static/img/profession de foi de Benjamin/3.jpg'}
+        {title: '1', url: '../../static/img/profession de foi de Benjamin/1.JPG'},
+        {title: '2', url: '../../static/img/profession de foi de Benjamin/2.JPG'},
+        {title: '3', url: '../../static/img/profession de foi de Benjamin/3.JPG'}
       ],
       [
         {title: '1', url: '../../static/img/Soirée/1.jpg'},
@@ -167,31 +167,31 @@ export default {
         {title: '3', url: '../../static/img/Soirée/3.jpg'}
       ],
       [
-        {title: 'Cocktail mises en bouche', url: '../../static/img/un monde de saveurs/1.jpg'},
-        {title: 'Cuillère garnie', url: '../../static/img/un monde de saveurs/2.jpg'},
-        {title: 'Mousse de fromage blanc sur sablé vanille, caramel', url: '../../static/img/un monde de saveurs/3.jpg'},
-        {title: 'Mousse de fromage blanc sur sablé vanille, compote de rhubarbe à la grenadine', url: '../../static/img/un monde de saveurs/4.jpg'},
-        {title: 'Noisette de boeuf en croûte de poivre', url: '../../static/img/un monde de saveurs/5.jpg'},
-        {title: "Saumon rôti, tombée d'épinards et champignons", url: '../../static/img/un monde de saveurs/6.jpg'},
-        {title: 'verrine de pennes, tomates et olives', url: '../../static/img/un monde de saveurs/7.jpg'},
-        {title: "Verrine fromage d'enfance au curry, brunoise de pommes", url: '../../static/img/un monde de saveurs/8.jpg'}
+        {title: 'Cocktail mises en bouche', url: '../../static/img/un monde de saveurs/1.JPG'},
+        {title: 'Cuillère garnie', url: '../../static/img/un monde de saveurs/2.JPG'},
+        {title: 'Mousse de fromage blanc sur sablé vanille, caramel', url: '../../static/img/un monde de saveurs/3.JPG'},
+        {title: 'Mousse de fromage blanc sur sablé vanille, compote de rhubarbe à la grenadine', url: '../../static/img/un monde de saveurs/4.JPG'},
+        {title: 'Noisette de boeuf en croûte de poivre', url: '../../static/img/un monde de saveurs/5.JPG'},
+        {title: "Saumon rôti, tombée d'épinards et champignons", url: '../../static/img/un monde de saveurs/6.JPG'},
+        {title: 'verrine de pennes, tomates et olives', url: '../../static/img/un monde de saveurs/7.JPG'},
+        {title: "Verrine fromage d'enfance au curry, brunoise de pommes", url: '../../static/img/un monde de saveurs/8.JPG'}
       ],
       [
-        {title: '1', url: '../../static/img/un monde de saveurs en 2012/1.jpg'},
-        {title: '2', url: '../../static/img/un monde de saveurs en 2012/2.jpg'},
-        {title: '3', url: '../../static/img/un monde de saveurs en 2012/3.jpg'},
-        {title: '4', url: '../../static/img/un monde de saveurs en 2012/4.jpg'},
-        {title: '5', url: '../../static/img/un monde de saveurs en 2012/5.jpg'},
-        {title: '6', url: '../../static/img/un monde de saveurs en 2012/6.jpg'},
-        {title: '7', url: '../../static/img/un monde de saveurs en 2012/7.jpg'}
+        {title: '1', url: '../../static/img/un monde de saveurs en 2012/1.JPG'},
+        {title: '2', url: '../../static/img/un monde de saveurs en 2012/2.JPG'},
+        {title: '3', url: '../../static/img/un monde de saveurs en 2012/3.JPG'},
+        {title: '4', url: '../../static/img/un monde de saveurs en 2012/4.JPG'},
+        {title: '5', url: '../../static/img/un monde de saveurs en 2012/5.JPG'},
+        {title: '6', url: '../../static/img/un monde de saveurs en 2012/6.JPG'},
+        {title: '7', url: '../../static/img/un monde de saveurs en 2012/7.JPG'}
       ],
       [
-        {title: '1', url: '../../static/img/Une prestation/1.jpg'},
-        {title: '2', url: '../../static/img/Une prestation/2.jpg'},
-        {title: '3', url: '../../static/img/Une prestation/3.jpg'},
-        {title: '4', url: '../../static/img/Une prestation/4.jpg'},
-        {title: '5', url: '../../static/img/Une prestation/5.jpg'},
-        {title: '6', url: '../../static/img/Une prestation/6.jpg'}
+        {title: '1', url: '../../static/img/Une prestation/1.JPG'},
+        {title: '2', url: '../../static/img/Une prestation/2.JPG'},
+        {title: '3', url: '../../static/img/Une prestation/3.JPG'},
+        {title: '4', url: '../../static/img/Une prestation/4.JPG'},
+        {title: '5', url: '../../static/img/Une prestation/5.JPG'},
+        {title: '6', url: '../../static/img/Une prestation/6.JPG'}
       ]
       ]
     }
