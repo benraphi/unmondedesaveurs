668651d32ea537a71a6dd0e7fd874f8aef481a59
Benjamin Bonvalet <benjamin591997@hotmail.fr>
2019-02-15 18:38:46 +0100

    test google

---
+++ src/components/SiteMap.vue
@@ -0,0 +1,3 @@
+<template>
+    <a href="../../sitemap.txt"> Téléchargez </a>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,7 @@ const routerOptions = [
   { path: '/menu/envie', component: 'MenuEnvie', name: 'menuenvie' },
   { path: '/menu/saveurs', component: 'MenuSaveurs', name: 'menusaveurs' },
   { path: '/photosprestation', component: 'PhotosPrestations', name: 'photosprestations' },
+  { path: '/sitemap', component: 'SiteMap', name: 'sitemap' },
   { path: '*', component: 'NotFound' }
 ]
 const routes = routerOptions.map(route => {
